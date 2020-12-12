fcd18cb48273444e34fb2041fb0bf8c37967c95d
DimonMakedonov <59868173+DimonMakedonov@users.noreply.github.com>
2020-12-12 14:23:33 +0300

    1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FastReport;
+using FastReport.Export.Pdf;
 
 namespace ConsoleApp4
 {
     class Program
     {
-
-        RichObject rich;
-
         static void Main(string[] args)
         {
+            // create report instance
+            Report report = new Report();
+
+            // load the existing report
+            report.Load(@"C:\Users\Dima\Desktop\FR\frx\1.frx");
+
+            // run the report
+            report.Prepare();
+
+            // create export instance
+            FastReport.Export.Pdf.PDFExport export = new PDFExport();
+
+            // export the report
+            report.Export(export, "result.pdf");
+
+            // free resources used by report
+            report.Dispose();
         }
     }
 }
